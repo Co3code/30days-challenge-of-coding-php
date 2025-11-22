51afdf2f9a83ff94499f6460ef3d6261ce5d4a7a
Co3code <countian3@gmail.com>
2025-11-23 01:17:17 +0800

    Update .gitignore to exclude database and unnecessary files

--- day25/db.php
+++ day25/db.php
@@ -1,11 +1,1 @@
-<?php
-$db = new PDO("sqlite:database.sqlite");
-
-// Create table automatically if not exists
-$db->exec("
-    CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        email TEXT NOT NULL
-    );
-");
+                 
