8f9c8ed310fd6661cf1e0a832756803065d2a363
Co3code <countian3@gmail.com>
2025-11-23 21:26:07 +0800

    sending email with phpmailer

--- day25/db.php
+++ day25/db.php
@@ -1,1 +1,13 @@
-                 
+<?php
+$db = new PDO("sqlite:database.sqlite");
+
+// Create table automatically if not exists
+//exec means “execute this SQL command.”(Execute = run)
+$db->exec("
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL
+    );
+");
+//null means must have a value. It cannot be empty.
